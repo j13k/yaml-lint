9d18ef1e5f1980d38e8a2bed0611d1e99af95df3
John Fitzpatrick <jfitzpatrick@datalink.com.au>
2020-04-09 19:01:33 +1000

    Introduced directory structure for classes

--- src/J13k/YamlLint/UsageException.php
+++ src/J13k/YamlLint/UsageException.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 
 namespace J13k\YamlLint;
 
+use RuntimeException;
+
 /**
  * Runtime exception for triggering usage message.
  *
  * @property int $code Exception code is passed through as script exit code
  */
-class UsageException extends \RuntimeException
+class UsageException extends RuntimeException
 {
 }
