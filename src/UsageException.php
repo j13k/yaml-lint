e9943174f7b345628a81b71751c7b173f0c279e1
John Fitzpatrick <jfitzpatrick@datalink.com.au>
2020-04-09 18:01:14 +1000

    *** WIP ***

--- src/UsageException.php
+++ src/UsageException.php
@@ -1,9 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace J13k\YamlLint;
 
 /**
- * Runtime exception for triggering usage message
+ * Runtime exception for triggering usage message.
  *
  * @property int $code Exception code is passed through as script exit code
  */
