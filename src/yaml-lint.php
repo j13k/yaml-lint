deaf9db1b253d445cd879641b232b77ceba01e32
John Fitzpatrick <jfitzpatrick@datalink.com.au>
2021-03-11 17:34:33 +1100

    Version 1.1.4

--- src/yaml-lint.php
+++ src/yaml-lint.php
@@ -14,7 +14,7 @@ use Symfony\Component\Yaml\Exception\ParseException;
 use Symfony\Component\Yaml\Yaml;
 
 define('APP_NAME', 'yaml-lint');
-define('APP_VERSION', '1.1.x-dev');
+define('APP_VERSION', '1.1.4');
 
 define('ANSI_BLD', 01);
 define('ANSI_UDL', 04);
